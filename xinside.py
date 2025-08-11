b28ea930133cc1f26c18a8c8cc5d4b46ef290d14
Borya <borya@example.com>
2025-08-11 13:11:58 +0800

    Add interactive mode and replace model

--- xinside.py
+++ xinside.py
@@ -4,7 +4,8 @@ import io
 import os
 from openai import OpenAI
 
-data_file_path = 'data/freelancer_earnings_bd.csv'
+data_file_path = input("Введите путь к *.csv файлу с данными: ") # 'data/freelancer_earnings_bd.csv'
+
 df_pandas = pd.read_csv(data_file_path)
 
 df_decribe = io.StringIO()
@@ -12,50 +13,55 @@ df_pandas.info(buf=df_decribe)
 info = df_decribe.getvalue()
 
 api_key = os.getenv("XINSIDE_OPENROUTER_KEY")
-
-query = input("Введите запрос к данным: ")
-
-assert query.strip() != "", "Запрос не может быть пустым."
-
-prompt_polars = f"""У тебя есть следующее описание датафрейма Pandas с данными "{info}". 
-Датафрейм хранится в переменной `df`.
-Также у тебя есть следующий запрос пользователя на естественном языке:
-"{query}".
-
-Выведи запрос на SQL, который можно передать в метод `sql` библиотеки polars, чтобы получить ответ на запрос пользователя.
-Выведи ТОЛЬКО код на SQL, без каких-либо пояснений.""" 
-
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
   api_key=api_key,
 )
 
-print("Отправка запроса к LLM...")
+while True:
+  query = input("Введите запрос к данным, либо quit для выхода: ")
 
-completion = client.chat.completions.create(
-  extra_headers={
-  },
-  extra_body={},
-  model="qwen/qwen3-32b:free",
-  messages=[
-    {
-      "role": "user",
-      "content": prompt_polars
-    }
-  ],
-  temperature=0.0,
-)
+  assert query.strip() != "", "Запрос не может быть пустым."
+  if query == "quit":
+     break
 
-df = pl.read_csv(data_file_path)
+  prompt_polars = f"""У тебя есть следующее описание датафрейма Pandas с данными "{info}". 
+  Датафрейм хранится в переменной `df`.
+  Также у тебя есть следующий запрос пользователя на естественном языке:
+  "{query}".
 
-sql_query = completion.choices[0].message.content.strip()
-print("Сгенерированный SQL запрос:")
-print(sql_query)
+  Выведи запрос на SQL, который можно передать в метод `sql` библиотеки polars, чтобы получить ответ на запрос пользователя.
+  Выведи ТОЛЬКО код на SQL, без каких-либо пояснений.""" 
 
-try:
-    output = pl.sql(sql_query).collect()
-    print("Ответ на запрос:")
-    print(output)
-except Exception as e:
-    print(f"Ошибка! Попробуйте переформулировать вопрос.")
-    exit(1)
+
+  print("Отправка запроса к LLM...")
+
+  completion = client.chat.completions.create(
+    extra_headers={
+    },
+    extra_body={},
+    model="qwen/qwen-2.5-coder-32b-instruct:free",
+    messages=[
+      {
+        "role": "user",
+        "content": prompt_polars
+      }
+    ],
+    temperature=0.0,
+  )
+
+
+
+  df = pl.read_csv(data_file_path)
+
+  sql_query = completion.choices[0].message.content.strip().strip('```sql').strip('```')
+  print("Сгенерированный SQL запрос:")
+  print(sql_query)
+
+  try:
+      output = pl.sql(sql_query).collect()
+      print("Ответ на запрос:")
+      print(output)
+  except Exception as e:
+      print(e)
+      print(f"Ошибка! Попробуйте переформулировать вопрос.")
